882ed81e2118ad9adf4eb1d32207589334d015c5
Beth <bethnoser@gmail.com>
2019-08-26 20:23:27 -0500

    new log form and methods

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,5 +1,41 @@
 class LogController < ApplicationController
+  before_action :find_log, only: [:show, :edit, :update, :destroy]
   def index
   end
 
+  def show
+
+  end
+
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(log_params)
+    if @log.save
+      redirect_to @log
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:date, :book, :minutes)
+  end
+
+  def find_log
+    @log = Log.find(params[:id])
+  end
 end
